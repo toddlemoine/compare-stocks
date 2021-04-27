4de55d7610d7d4bb572509349795d118fee72856
Todd Lemoine <todd.lemoine@gmail.com>
2021-04-26 21:19:57 -0700

    Simple starter page, scaffolding

---
+++ src/components/App.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("has title", () => {
+  render(<App />);
+  const heading = screen.getByRole("heading", /Stock Comparison/i);
+  expect(heading).toBeInTheDocument();
+});
